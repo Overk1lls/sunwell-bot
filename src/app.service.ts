eaeb716c8364dfd708654def38505e272f348e3a
Yurii Pakhota <yuriosity.ufo@gmail.com>
2023-07-27 01:37:34 +0300

    Create & start the Discord bot

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Client, Collection, Events, GatewayIntentBits, Partials, REST, Routes } from 'discord.js';
 @Injectable()
 export class AppService implements OnModuleInit {
+  private discordClient: Client;
+  private readonly commands = new Collection<string, Interaction>();
   private readonly logger = new Logger(AppService.name);
 
   constructor(
@@ -9,7 +12,29 @@ export class AppService implements OnModuleInit {
   ) {}
 
   async onModuleInit() {
+    this.discordClient = new Client({
+      partials: [Partials.User, Partials.GuildMember, Partials.Message],
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
+      presence: {
+        status: 'online',
+      },
+    });
+
+    this.initBot();
   }
 
+  async initBot() {
+    const token = this.configService.getOrThrow<string>('DISCORD_BOT_TOKEN');
+
+    await this.discordClient.login(token);
+
+    this.discordClient.on(Events.ClientReady, () =>
+      this.logger.log(`${this.discordClient.user.username} has been bootstrapped!`),
+    );
   }
 
